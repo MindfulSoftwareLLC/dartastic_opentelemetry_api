4d2ac2649b6d824909fd1f054ef642cf18ff724e
yusuf <myusufrosman@gmail.com>
2025-11-15 22:33:21 +0800

    feat: add LogRecord interface

--- lib/src/api/logs/log_record.dart
+++ lib/src/api/logs/log_record.dart
@@ -1,27 +1,34 @@
-enum SeverityNumber {
-  unspecified,
-  trace,
-  trace2,
-  trace3,
-  trace4,
-  debug,
-  debug2,
-  debug3,
-  debug4,
-  info,
-  info2,
-  info3,
-  info4,
-  warn,
-  warn2,
-  warn3,
-  warn4,
-  error,
-  error2,
-  error3,
-  error4,
-  fatal,
-  fatal2,
-  fatal3,
-  fatal4,
+import '../common/attributes.dart';
+import '../context/context.dart';
+import 'severity.dart';
+
+/// Required parameter for LogRecord. According to the spec
+/// in https://opentelemetry.io/docs/specs/otel/logs/api/#emit-a-logrecord
+///
+/// The API MUST accept the following parameters:
+///
+/// Timestamp (optional)
+/// Observed Timestamp (optional)
+/// The Context associated with the LogRecord. When implicit Context is supported, then this parameter SHOULD be optional and if unspecified then MUST use current Context. When only explicit Context is supported, this parameter SHOULD be required.
+/// Severity Number (optional)
+/// Severity Text (optional)
+/// Body (optional)
+/// Attributes (optional)
+/// Event Name (optional)
+abstract class LogRecord {
+  DateTime? get timestamp;
+
+  DateTime? get observedTimestamp;
+
+  Context? get context;
+
+  Severity? get severityNumber;
+
+  String? get severityText;
+
+  dynamic get body;
+
+  Attributes? get attributes;
+
+  String? get eventName;
 }
